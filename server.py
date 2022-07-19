4ec190f46c849c9c86fd961accad16be9408c38f
DESKTOP-MLLLST5\Julian <kidsfreej@gmail.com>
2022-07-19 15:26:08 -0400

    Finished Edit

--- server.py
+++ server.py
@@ -6,19 +6,16 @@ from flask import request
 app = Flask(__name__)
 
 
-teams = []
+teams = {}
 
 @app.route("/submitnewteam",methods=["POST"])
 def add_new_team():
     t = Team(request.form["fullName"],request.form["shortName"],request.form["division"],request.form["practiceDays"],request.form["homeFacility"],request.form["alternativeFacility"],request.form["noPlayDates"],request.form["noMatchDays"],request.form["homeMatchPCT"], request.form["startDate"])
     if len(t.errors)>0:
         return render_template("submitnewteam_fail.html",errors=t.errors)
-
-    for team in teams:
-        print(team.fullName,t.fullName)
-        if team.fullName == t.fullName:
-            return render_template("submitnewteam_fail.html",errors=[team.fullName.value+" is already a team!"])
-    teams.append(t)
+    if t.fullName.value in teams:
+        return render_template("submitnewteam_fail.html",errors=[t.fullName.value+" is already a team!"])
+    teams[t.fullName.value] = t
 
     return render_template("submitnewteam.html",data=t.properties)
 @app.route("/newteam")
@@ -33,11 +30,21 @@ def edit_page():
     arg = request.args.get("team")
     if arg==None:
         return render_template("selectteamedit.html",teams=teams)
-    for t in teams:
-        if t.fullName==arg:
-            return render_template("editteam.html",team=t)
-    return f"<a href='/'>Home</a><br><h1>ERROR</h1>{html.escape(arg)} does not exist<br><a href='edit'>Edit</a>"
-@app.route("/submitedit"):
 
-def editor_page
+    if arg in teams:
+        return render_template("editteam.html",team=teams[arg])
+    return f"<a href='/'>Home</a><br><h1>ERROR</h1>{html.escape(arg)} does not exist<br><a href='edit'>Edit</a>"
+@app.route("/submitedit",methods=["POST"])
+def editor_page():
+    name  = request.form["teamname"]
+    if name in teams:
+
+        t= Team(request.form["fullName"],request.form["shortName"],request.form["division"],request.form["practiceDays"],request.form["homeFacility"],request.form["alternativeFacility"],request.form["noPlayDates"],request.form["noMatchDays"],request.form["homeMatchPCT"], request.form["startDate"])
+
+        if len(t.errors) > 0:
+            return render_template("submitnewteam_fail.html", errors=t.errors)
+        teams.pop(request.form["teamname"])
+        teams[t.fullName.value] = t
+        return render_template("submiteditteam.html",data=t.properties)
+    return "Ok  this should neve everr happen. "
 app.run()
