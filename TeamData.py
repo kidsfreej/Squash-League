8fa5a43390bd3f5c8d3eb9eb3aa7e3a4daedb37d
DESKTOP-MLLLST5\Julian <kidsfreej@gmail.com>
2022-07-19 10:32:51 -0400

    asdsadsa

--- TeamData.py
+++ TeamData.py
@@ -8,12 +8,12 @@ properties = ["Team Name", "Abbreviated Name", "Division", "Practice Days", "Hom
 class Prop:
     def __init__(self,name:str,value):
         self.name = name
-        self.value = value
+        self.value = value.strip()
         self.error = False
     def __repr__(self):
         return repr(self.value)
 class Date:
-    def __int__(self,name:str,value:str):
+    def __init__(self,name:str,value:str):
         self.name = name
         self.error=  False
         spled = value.split("/")
@@ -23,35 +23,59 @@ class Date:
             self.year = int(spled[2])
         except:
             self.error=True
+            self.value = value
     def __repr__(self):
         return f"{self.month}/{self.day}/{self.year}"
 class Dates:
-    def __int__(self,name:str,value:str):
+    def __init__(self,name:str,value:str):
         self.name =name
         self.error = False
-        split = value.split(" ")
+        split = value.strip().split(",")
+        self.dates = []
         try:
-            for sp
-            self.day = int(spled[1])
-            self.month =int(spled[0])
-            self.year = int(spled[2])
+            for sp in split:
+                self.dates.append(Date(name,sp.strip()))
         except:
             self.error=True
-
+            self.value = value
+class Number:
+    def __init__(self,name:str,value:str):
+        self.error = False
+        self.name = name
+        try:
+            self.value = float(value.strip())
+        except:
+            self.error =True
+            self.value = value
 class Team:
     def __init__(self, fullName, shortName, division, practiceDays, homeFacility,
                  alternateFacility, noPlayDates, noMatchDays, homeMatchPCT, startDate):
         self.fullName = Prop("Full Name",fullName)
         self.shortName = Prop("Short Name",shortName)
         self.division = Prop("Division",division)
-        self.practiceDays = Prop("Practice Days", practiceDays)
+        self.practiceDays = Dates("Practice Days", practiceDays)
         self.homeFacility = Prop("Home Facility",homeFacility)
         self.alternateFacility = Prop("Alternate Facility",alternateFacility)
-        self.noPlayDates = Prop("No Play Dates",noPlayDates)
-        self.noMatchDays = Prop("No Match Days",noMatchDays)
-        self.homeMatchPCT = Prop("Home Match %",homeMatchPCT)
-        self.startDate = Prop("Start Date",startDate)
+        self.noPlayDates = Dates("No Play Dates",noPlayDates)
+        self.noMatchDays = Dates("No Match Days",noMatchDays)
+        self.homeMatchPCT = Number("Home Match %",homeMatchPCT)
+        self.startDate = Date("Start Date",startDate)
         self.properties = [self.fullName,self.shortName,self.division,self.practiceDays,self.homeFacility,self.alternateFacility,self.noPlayDates,self.noMatchDays,self.homeMatchPCT,self.startDate]
+        errors = []
+        for prop in self.properties:
+            if prop.error:
+                m = f"{prop.name}: "
+                if type(prop)==Number:
+                    m+=f"Make sure to enter a number (no other symbols). You entered '{prop.value}'."
+                elif type(prop)==Date:
+                    m+=f"Make sure to enter a date (mm/dd/yy). You entered '{prop.value}'"
+                elif type(prop)==Dates:
+                    k = 'and'.join([f"'{d}'" for d in prop.value.split()])
+                    m+=f"Make sre you enter common seperated date(s). You entered {k}"
+                else:
+                    raise NotImplementedError("oopsies")
+                errors.append(m)
+        self.errors = errors
     # def __str__(self):
     #     s = ""
     #     for prop in self.properties:
