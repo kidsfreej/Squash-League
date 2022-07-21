98f83db5f445ba62998911e5c995404fe0f12745
DESKTOP-MLLLST5\Julian <kidsfreej@gmail.com>
2022-07-21 14:29:39 -0400

    finished dates working on edit for dates

--- TeamData.py
+++ TeamData.py
@@ -1,3 +1,4 @@
+import datetime
 import html
 
 # TODO Web page (flask) as input/UI
@@ -20,7 +21,13 @@ class Prop:
             raise Exception("uh oh")
         return self.value==other.value
 class Date:
-    def __init__(self,name:str,value:str):
+    def __init__(self,name:str,value:str,month=-1,day=-1,year=-1):
+        self.error = False
+        if month !=-1:
+            self.month = month
+            self.year = year
+            self.day = day
+            return
         self.name = name
         self.error=  False
         spled = value.split("/")
@@ -35,10 +42,13 @@ class Date:
         return f"{self.month}/{self.day}/{self.year}"
     def __eq__(self, other):
         return self.month == other.month and self.day == other.day and self.year == other.year
+
 class Dates:
     def __init__(self,name:str,value:str):
-        self.name =name
         self.error = False
+        self.name =name
+        self.date_ranges = []
+        self.repr_dates = []
         split = value.strip().split(",")
         self.dates = []
         if len(split)==1 and split[0]=="":
@@ -48,18 +58,61 @@ class Dates:
             for sp in split:
                 ranged = sp.split("-")
                 if len(ranged)>1:
+                    if len(ranged)>2:
+                        raise Exception("error")
+                    self.date_ranges.append (( Date("",ranged[0]),Date("", ranged[1])))
+                    cur_date =datetime.datetime.strptime(ranged[0].strip(),"%m/%d/%y")
+                    end_date = datetime.datetime.strptime(ranged[1].strip(),"%m/%d/%y")
+                    while cur_date!= end_date:
+                        self.dates.append(Date("","",cur_date.month, cur_date.day, cur_date.year))
+                        cur_date +=datetime.timedelta(days=1)
 
-                self.dates.append(Date(name,sp.strip()))
-                if self.dates[-1].error:
-                    self.error=True
-                    self.value = value
-                    break
-        except:
+                    self.dates.append(Date("","",cur_date.month,cur_date.day,cur_date.year))
+                else:
+                    self.dates.append(Date(name,sp.strip()))
+                    self.repr_dates.append(Date(name,sp.strip()))
+                    if self.dates[-1].error:
+                        print("here")
+                        self.error=True
+                        self.value = value
+                        break
+        except Exception as e:
+            print(e)
             self.error=True
             self.value = value
     def __repr__(self):
-        return ', '.join(map(str,self.dates))
 
+        return ', '.join(list(map(str,self.repr_dates))+[repr(x[0])+"-"+repr(x[1]) for x in self.date_ranges])
+class Weekday:
+    weekdays = ("sunday","monday","tuesday","wednesday","thursday","friday","saturday")
+    def __init__(self,name:str,value:str):
+        self.name = name
+        self.value = value
+        self.error = False
+
+    def __repr__(self):
+        return self.value[0].capitalize()+self.value[1:]
+    def __hash__(self):
+        return hash(self.value)
+    def __eq__(self, other):
+        if type(other)==str:
+            return self.value == other
+        return self.value == other.value
+class Weekdays:
+    def __init__(self,name:str,days,error=False):
+        self.name = name
+        self.days = days
+        self.error = error
+    def __repr__(self):
+        return ', '.join([repr(x) for x in self.days])
+    @staticmethod
+    def parse_weekdays(form,start):
+        days = []
+
+        for day in Weekday.weekdays:
+            if start+"-"+day in form:
+                days.append(Weekday("",day.lower()))
+        return days
 class Number:
     def __init__(self,name:str,value:str):
         self.error = False
@@ -73,6 +126,14 @@ class Number:
         return str(self.value)
     def __eq__(self, other):
         return self.value == other.value
+class Facility:
+    def __init__(self,year,fullName,shortName,start,end):
+        self.year =Prop("Divison Year", year)
+        self.fullName = Prop("Division Full Name",fullName)
+        self.shortName = Prop("Division Abbreviation",shortName)
+        self.start = Date("Division Start Date",start)
+        self.end = Date("Division End Date",end)
+
 
 class Team:
     def __init__(self, fullName, shortName, division, practiceDays, homeFacility,
@@ -80,11 +141,11 @@ class Team:
         self.fullName = Prop("Full Name",fullName)
         self.shortName = Prop("Short Name",shortName)
         self.division = Prop("Division",division)
-        self.practiceDays = Dates("Practice Days", practiceDays)
+        self.practiceDays = Weekdays("Practice Days", practiceDays)
         self.homeFacility = Prop("Home Facility",homeFacility)
         self.alternateFacility = Prop("Alternate Facility",alternateFacility)
         self.noPlayDates = Dates("No Play Dates",noPlayDates)
-        self.noMatchDays = Dates("No Match Days",noMatchDays)
+        self.noMatchDays = Weekdays("No Match Days",noMatchDays)
         self.homeMatchPCT = Number("Home Match %",homeMatchPCT)
         self.startDate = Date("Start Date",startDate)
         self.properties = [self.fullName,self.shortName,self.division,self.practiceDays,self.homeFacility,self.alternateFacility,self.noPlayDates,self.noMatchDays,self.homeMatchPCT,self.startDate]
